163116b72b672a3c0e303e4d0f204d1d5572c295
thetinshusasi <thetinshusasi@gmail.com>
2024-11-28 17:28:10 -0400

    feat(app): Ticket 6 MUI integration
    Added MUI Integration to the next js app
    
    Closes: Ticket 6

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,35 @@
 import './globals.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+import { Roboto } from 'next/font/google'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 export const metadata = {
-	title: 'NextJS + Electron Boilerplate',
+	title: 'NextJS  Electron Boilerplate',
 	description:
 		'A neat boilerplate for building Electron apps, with NextJS at the frontend and pre-configured with a bunch of handy development tools.',
 }
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+const roboto = Roboto({
+	weight: ['300', '400', '500', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-roboto',
+})
+
+export default function RootLayout(props) {
+	const { children } = props
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className={roboto.variable}>
+				<AppRouterCacheProvider>
+					<ThemeProvider theme={theme}>{children}</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
 		</html>
 	)
 }
